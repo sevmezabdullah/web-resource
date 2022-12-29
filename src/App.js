d78bd0b9db810c51ec8c81729973afbab9c62f28
sevmezabdullah <abdullahsevmez@gmail.com>
2022-12-29 09:48:57 +0300

    style eklendi

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import './App.css';
-
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>CSS Selectors</h1>
+      <h2>Article 1</h2>
+      <p className="red-paragraph">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, et
+        amet quaerat necessitatibus iusto sunt iure error consequatur
+        consectetur? Inventore, rerum soluta! Repellendus enim earum velit. Quo
+        sit a ipsa?
+      </p>
+      <h2>Article 2</h2>
+      <p className="italic-paragraph">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi numquam
+        atque commodi suscipit mollitia a veritatis deleniti sapiente porro non.
+        Soluta libero maxime provident pariatur dolorum magni incidunt quo
+        saepe?
+      </p>
+      <h2>Article 3</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+        recusandae nemo impedit, commodi ut nobis nihil veritatis architecto
+        ipsa officia nesciunt, alias quasi! Tempora qui molestias laborum
+        beatae? Eos, ut.
+      </p>
+    </div>
+  );
 }
 
 export default App;
